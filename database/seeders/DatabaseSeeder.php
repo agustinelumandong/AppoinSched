74d7b9d676723682c05a4037faf57b7cfda30f91
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2025-06-03 15:41:04 +0800

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+        DB::table('users')->delete();
+        User::factory()->create([
+            'first_name' => 'John',
+            'middle_name' => 'Doe',
+            'last_name' => 'Doe',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
