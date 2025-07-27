8139fe597caeaed9d7f7597747e33faeb8b7ea23
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2025-07-27 23:17:10 +0800

    Update application name and enhance UI components
    Changes the application name in the configuration to 'Requisition and Appointment System'. Updates the app logo and header in the Blade templates for improved branding. Adjusts the layout of the client dashboard to better organize the display of available offices and appointments, enhancing user experience.

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,6 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md  text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white " />
+<div class="flex aspect-square size-18 items-center justify-center rounded-md  text-accent-foreground">
+    <x-app-logo-icon class="size-96 fill-current text-white " />
 </div>
-<div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">eSantoTomas</span>
+<div class="ms-1 grid flex-1 text-start text-lg">
+    <span class="mb-0.5 truncate leading-none font-extrabold">Requisition and <br /> Appointment System</span>
 </div>
