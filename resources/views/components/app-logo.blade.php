f8b908d393762e0bc02d4ecbe2a1f3fcc72fffc9
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2025-07-28 09:25:46 +0800

    Update app logo text styling for improved emphasis

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -2,5 +2,5 @@
     <x-app-logo-icon class="size-96 fill-current text-white " />
 </div>
 <div class="ms-1 grid flex-1 text-start text-lg">
-    <span class="mb-0.5 truncate leading-none font-extrabold">Requisition and <br /> Appointment System</span>
+    <span class="mb-0.5 truncate leading-none font-bold">Requisition and <br /> Appointment System</span>
 </div>
