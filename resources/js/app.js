b8142da173244c732d7b01292afad21fd066e760
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2025-10-09 01:05:14 +0800

    Add QR code generator dependencies and integrate into app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../css/app.css'; // or your main CSS file
+// Import QRCode generator
+import qrcode from 'qrcode-generator';
 
+// Make it available globally
+window.qrcode = qrcode;
